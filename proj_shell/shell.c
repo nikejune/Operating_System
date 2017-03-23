8930707bfd319bd7bd8a96f81254615fb29a1285
leegijun <dmlguse322@hanyang.ac.kr>
2017-03-24 01:53:17 +0900

    hungry

--- proj_shell/shell.c
+++ proj_shell/shell.c
@@ -6,17 +6,23 @@ int main(int argc, char * argv[])
 {
     int rc =fork();
     if(rc< 0){
-        fprintf(atderr, "fork failed\n");
+        fprintf(stderr, "fork failed\n");
         exit(1);
     }
     else if(rc == 0) //child
     {
-        char * myargs[3];
+ //       char* str[20]={"ls","-al",NULL};
+        
+        char* str1[20]={"cd","..",NULL};
+        char* str2[20]={"pwd",NULL};
+ //       char * myargs;
+        execvp(str1[0],str1);
+
 
     }else // parent
     {
         int wc = wait(NULL);
-        
+        printf("what\n");
     }
 
     return 0;
