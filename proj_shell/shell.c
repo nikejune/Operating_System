276a0c6fbe02864e799b0b2d4a05bbeec7bf657e
leegijun <dmlguse322@hanyang.ac.kr>
2017-03-24 23:11:56 +0900

    interactive mode v1.01

--- proj_shell/shell.c
+++ proj_shell/shell.c
@@ -4,75 +4,101 @@
 #include <string.h>
 int main(int argc, char * argv[])
 {
-    int rc =fork();
-    int* rcArr;
-    int* wcArr;
-    if(rc< 0){
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    }
-    else if(rc == 0) //child
-    {
-        char str[50];
-        char* ptr;
-        char* stmtArr[50];
-        char* strArr[50];
-        int idx=0;
-        int numOfStmt;
-        int i;
-
-        fgets(str, sizeof(str), stdin);
-        str[strlen(str)-1] = NULL;
-  
-        ptr = strtok(str,";");
-        while(ptr !=NULL)
-        {
-            stmtArr[idx++]= ptr;
-            ptr = strtok(NULL, ";");
-        
+    //interactive mode
+    if(argc ==1){
+        int* rcArr;
+        int* wcArr;
+        int rc =fork();
+        if(rc< 0){
+            fprintf(stderr, "fork failed\n");
+            exit(1);
         }
-        numOfStmt = idx;
-
-        for(i=0; i<numOfStmt; i++)
+        else if(rc == 0) //child
         {
-            rcArr = (int*)malloc(sizeof(int)*numOfStmt);
-            wcArr = (int*)malloc(sizeof(int)*numOfStmt);
+            char str[50];
+            char* ptr;
+            char* stmtArr[50];
+            char* strArr[50];
+            int idx;
+            int numOfStmt;
+            int i;
 
-            rcArr[i] = fork();
-            if(rcArr[i]<0)
-            {
-                fprintf(stderr, "fork failed\n");
-                exit(1);
-            }
-            else if(rcArr[i]==0)
-            {
-                idx = 0;
-                ptr = strtok(stmtArr[i], " ");
+            while(1){
+                idx =0;
         
+                printf("prompt> ");
+                fgets(str, sizeof(str), stdin);
+                str[strlen(str)-1] = NULL;
+          
+                if(!strcmp(str, "quit"))
+                { 
+         //           printf("bye");
+                    break;
+                }
+                ptr = strtok(str,";");
                 while(ptr !=NULL)
                 {
-                    strArr[idx++]= ptr;
-                    ptr = strtok(NULL, " ");
+                    stmtArr[idx++]= ptr;
+                    ptr = strtok(NULL, ";");
                 
                 }
-                strArr[idx] = NULL;
-                execvp(strArr[0], strArr);
-            }
-            else
-            {
-                wcArr[i] = wait(NULL);    
-            }
+                numOfStmt = idx;
+        
+                for(i=0; i<numOfStmt; i++)
+                {
+                    rcArr = (int*)malloc(sizeof(int)*numOfStmt);
+                    wcArr = (int*)malloc(sizeof(int)*numOfStmt);
+        
+                    rcArr[i] = fork();
+                    if(rcArr[i]<0)
+                    {
+                        fprintf(stderr, "fork failed\n");
+                        exit(1);
+                    }
+                    else if(rcArr[i]==0)
+                    {
+                        idx = 0;
+                        ptr = strtok(stmtArr[i], " ");
+                
+                        while(ptr !=NULL)
+                        {
+                            strArr[idx++]= ptr;
+                            ptr = strtok(NULL, " ");
+                        
+                        }
+                        strArr[idx] = NULL;
+                        execvp(strArr[0], strArr);
+                    }
+                    else
+                    {
+                        wcArr[i] = wait(NULL);
+                       printf("%dth fork dead\n", i+1);
 
+                    }
+        
+                }
+            }
+        }else // parent
+        {
+            int wc = wait(NULL);
+ //          printf("endend\n");
         }
-//        char* str1[20]={"cd","..",NULL};
-//        char* str2[20]={"pwd",NULL};
-//        execvp(str1[0],str1);
-    }else // parent
-    {
-        int wc = wait(NULL);
-        printf("what\n");
     }
+    //batch mode
+    else if(argc==2) 
+    {
+        FILE * fp = fopen(argv[1], "r");
+        if(fp == NULL)
+        {
+            fprintf(stderr, "file open fail");
+            exit(1);
+        
+        }
 
+
+
+
+    }
     return 0;
 
     
