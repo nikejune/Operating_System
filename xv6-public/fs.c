de79d6266bcfa8879513cc34ad359f5748f61703
leegijun <dmlgus322@hanyang.ac.kr>
2017-06-17 17:41:40 +0900

    filesystem complete

--- xv6-public/fs.c
+++ xv6-public/fs.c
@@ -378,6 +378,30 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
+  bn -= NINDIRECT;
+
+  if(bn < NDINDIRECT){
+    // Load indirect block, allocating if necessary.
+    if((addr = ip->addrs[NDIRECT+1]) == 0)
+      ip->addrs[NDIRECT+1] = addr = balloc(ip->dev);
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[bn/NINDIRECT]) == 0){
+      a[bn/NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[bn%NINDIRECT]) == 0){
+      a[bn%NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
+
+
   panic("bmap: out of range");
 }
 
@@ -391,7 +415,7 @@ itrunc(struct inode *ip)
 {
   int i, j;
   struct buf *bp;
-  uint *a;
+  uint *a, *b;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -410,7 +434,32 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+
   }
+
+  if(ip->addrs[NDIRECT+1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT+1]);
+    a = (uint*)bp->data;
+    for(i = 0; i < NINDIRECT; i++){
+        if(a[i]){
+            bp = bread(ip->dev, a[i]);
+            b = (uint*)bp->data;
+            for(j = 0; j < NINDIRECT; j++){
+                if(b[j])
+                    bfree(ip->dev, b[j]);
+            }
+            brelse(bp);
+            bfree(ip->dev, a[i]);
+            a[i] = 0;
+       }
+    }
+    cprintf("for loop escaped");
+//    brelse(bp);
+//    bfree(ip->dev, ip->addrs[NDIRECT+1]);
+    ip->addrs[NDIRECT+1] = 0;
+ 
+  }
+
 
   ip->size = 0;
   iupdate(ip);
